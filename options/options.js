5996610f5b24f27f0e1d1bef2221c0b7044e1dd3
django <django@4facd6820a0e>
2024-03-21 19:50:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "abd-240321-l2-6",
+  url: "https://abd-240321-l2-6.botics.co",
+  api: "https://abd-240321-l2-6.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
